ff6094f1c57509ee92ad46d6dff67e5a9ec7f874
Sultan Mahmud Ekram <smahmud1098@gmail.com>
2022-02-17 03:28:31 +0600

    Update balance

--- js /javascript.js
+++ js /javascript.js
@@ -16,3 +16,30 @@ function resetValue() {
     document.getElementById("rentField").value = ""
     document.getElementById("saveField").value = ""
 }
+
+document.getElementById("calculateBtn").addEventListener("click", function () {
+
+    const incomeField = fieldValue("incomeField")
+    const foodField = fieldValue("foodField")
+    const clothField = fieldValue("clothField")
+    const rentField = fieldValue("rentField")
+
+    // update calculations
+    if (incomeField && foodField && clothField && rentField) {
+        let totalExpenses = parseFloat(document.getElementById("totalExpenses").innerText)
+
+        totalExpenses = totalExpenses + foodField + clothField + rentField
+
+        if (totalExpenses > incomeField) {
+            alert("you cant spend more then your income")
+        }
+        else {
+            // update expnses
+            document.getElementById("totalExpenses").innerText = totalExpenses
+            // update balances
+            document.getElementById("totalBalance").innerText  = incomeField - totalExpenses
+        }
+    }
+
+    resetValue()
+})
